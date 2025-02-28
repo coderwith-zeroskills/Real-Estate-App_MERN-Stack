ef3dd38b93466b9e6d407344bada03d89409bd24
nikhil sacena <nikhilhill19@gmail.com>
2025-03-01 00:05:12 +0530

    register and login working

--- api/app.js
+++ api/app.js
@@ -1,11 +1,20 @@
 import express from "express";
 import cookie from "cookie-parser";
+import cors from "cors";
 import postRoutes from "./routes/post.route.js";
 import authRoutes from "./routes/auth.route.js";
 const app = express();
 
+app.use(cors({
+  origin: "http://localhost:5173", // Replace with your frontend URL
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+}));
+
 app.use(express.json())
 app.use(cookie())
+// Allow all origins
 app.use("/api/posts", postRoutes);
 app.use("/api/auth", authRoutes);
 
